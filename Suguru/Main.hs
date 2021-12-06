8ab11a9c17499fc10ec08969a766e19eef8daa39
murilloguindani <murilloguindani@gmail.com>
2021-12-06 18:00:09 -0300

    adicionado input do usuario para teste de celula

--- Suguru/Main.hs
+++ Suguru/Main.hs
@@ -1,13 +1,24 @@
 module Main (main) where
 
-    import System.IO (readFile)
-    import Suguru.Board (boardFromText, getBlock)
-    import Suguru.Utils (printMatrix, getNeighbors)
-    import Suguru.Solver (getCellOptions, getBlockOptions)
-    
-    main = do
-        content <- readFile "Examples/1.txt"
-        let rows = lines content
-        let board = boardFromText rows
-        printMatrix board
-        print (getCellOptions board (4, 5))
+import Data.ByteString (getLine)
+import Suguru.Board (boardFromText, getBlock)
+import Suguru.Solver (getBlockOptions, getCellOptions)
+import Suguru.Utils (getNeighbors, printMatrix)
+import System.IO (readFile)
+
+main = do
+  content <- readFile "Examples/1.txt"
+  let rows = lines content
+  let board = boardFromText rows
+
+  print "Indexado a partir de 1"
+  print "Linha:"
+  is <- Prelude.getLine
+  print "Coluna:"
+  js <- Prelude.getLine
+
+  let i = (read is :: Int)
+  let j = (read js :: Int)
+
+  printMatrix board
+  print (getCellOptions board (i, j))
